b3aafea04ab440c5a710ead4da223e2d67d9af9a
Gleb Karpenko <glebkarpenko1@gmail.com>
2023-05-09 19:09:15 +0300

    add lab8

---
+++ Lab8/Lab8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq.Expressions;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string text = File.ReadAllText($@"{Environment.CurrentDirectory}\input.txt");
+        char[] symbols = text.ToCharArray();
+        List<char> list = symbols.ToList<char>();
+
+        Program main = new Program();
+        Console.WriteLine("\nВикористаємо масив.");
+        Console.WriteLine("Кількість прописних літер в файлі 'input.txt': " + main.countUppercase(symbols));
+        Console.WriteLine("\nВикористаємо список.");
+        Console.WriteLine("Кількість прописних літер в файлі 'input.txt': " + main.countUppercase(list));
+    }
+
+    int countUppercase(char[] symbols)
+    {
+        int count = 0;
+        foreach (char element in symbols)
+        {
+            if (Char.IsUpper(element))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    int countUppercase(List<char> list)
+    {
+        int count = 0;
+        foreach (char element in list)
+        {
+            if (Char.IsUpper(element))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
